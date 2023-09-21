01d5018a82e927664562bd0fa041a940b4ef65a6
Brian Ashman <brian.ashman@comcast.net>
2023-09-20 22:42:01 -0700

    Complete passwords lab

--- server/app.py
+++ server/app.py
@@ -18,26 +18,41 @@ class ClearSession(Resource):
 class Signup(Resource):
     
     def post(self):
-        json = request.get_json()
         user = User(
-            username=json['username'],
-            password_hash=json['password']
+            username=request.get_json()['username'],
+            _password_hash=request.get_json()['password']
         )
         db.session.add(user)
         db.session.commit()
+        
+        session['user_id'] = user.id
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session['user_id']).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        
+        session['user_id'] = user.id
+        return user.to_dict()
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
